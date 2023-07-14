6a21c94658388dcdb87087b387e82bb1ca3ee843
Flopiras <Flopiras@yahoo.it>
2023-07-14 14:56:16 +0200

    add html

---
+++ censured.php
@@ -0,0 +1,25 @@
+<?php
+
+// recupero parola da censurare
+$word = $_GET["word"];
+
+// recupero il paragrafo 
+$paragraph = $_GET["paragraph"];
+
+echo ($word);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
