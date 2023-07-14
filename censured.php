6804d1bf8ef558588bf0f85d806e3590f0860b3a
Flopiras <Flopiras@yahoo.it>
2023-07-14 15:39:45 +0200

    add bootstrap in php file

--- censured.php
+++ censured.php
@@ -17,6 +17,9 @@ $new_word = str_replace($word, "***", $paragraph)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
